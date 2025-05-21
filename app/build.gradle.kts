b5def1ca9869fcfa13630aadf8f7aa7d7497985c
Volodymyr Khmelnytskiy <igel4@ukr.net>
2025-05-21 16:49:29 +0300

    Incremented app version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.vladimir_x.easyenglishlearn"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
